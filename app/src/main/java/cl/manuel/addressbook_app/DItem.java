180ff5350a991c69ca0b241acfd73b140ce7b6af
Manuel <manuel.vargas.t@ug.uchile.cl>
2018-09-05 15:26:58 -0300

    Fin construcción de GUIs + construcción clase DItem

--- app/src/main/java/cl/manuel/addressbook_app/DItem.java
+++ app/src/main/java/cl/manuel/addressbook_app/DItem.java
@@ -1,4 +1,54 @@
 package cl.manuel.addressbook_app;
 
-public class DItem {
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.drawable.Drawable;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+
+/*Clase que define los dividers entre los ítems de RecyclerView.
+ * Ver sample de implementación: bit.ly/DividerItemDecoration*/
+
+public class DItem extends RecyclerView.ItemDecoration{
+
+    private final Drawable divider;
+
+    //Constructor: carga lista de ítems divider de fábrica de Android
+    public DItem(Context context){
+
+        int[] attrs = {android.R.attr.listDivider};
+        divider = context.obtainStyledAttributes(attrs).getDrawable(0);
+    }
+
+    /*DIBUJAR LA LISTA DE ÍTEMS DIVIDER EN RECYCLERVIEW*/
+
+    /*A medida que el usuario scrollea a través de los ítems del RecyclerView, sus contenidos
+    van siendo redibujados para mostrar los ítems en sus nuevas posiciones en la pantalla.
+    Para ello, RecyclerView llama a onDrawOver() de su subclase RecyclerView.ItemDecoration.*/
+
+    /*onDrawOver() requiere: un Canvas para dibujar en el RecyclerView, el RecyclerView donde el
+    * Canvas dibujará, y el RecyclerView.State, que almacena info pasada entre varios componentes
+    * RecyclerView, aunque en esta ocasión solo se le pasa a super (el onDrawOver de la superclase).*/
+    @Override
+    public void onDrawOver(Canvas c, RecyclerView parent, RecyclerView.State state) {
+        super.onDrawOver(c, parent, state);
+
+        //Calcular coordenadas x (izq/der) de cada divider
+        int izq = parent.getPaddingLeft();
+        int der = parent.getWidth() - parent.getPaddingRight();
+
+        //Dibujar una línea debajo de cada ítem, salvo por el último (comuna)
+        for(int i = 0; i < parent.getChildCount() - 1; ++i){
+
+            View item = parent.getChildAt(i); //Obtener divider actual en iteración
+
+            //Calcular coordenadas y (arriba/abajo) de cada ítem divider
+            int arriba = item.getBottom() + ((RecyclerView.LayoutParams)item.getLayoutParams()).bottomMargin;
+            int abajo = arriba + divider.getIntrinsicHeight();
+
+            //Dibujar divider (línea) con los valores obtenidos
+            divider.setBounds(izq, arriba, der, abajo);
+            divider.draw(c);
+        }
+    }
 }
--- app/src/main/java/cl/manuel/addressbook_app/MainActivity.java
+++ app/src/main/java/cl/manuel/addressbook_app/MainActivity.java
@@ -17,29 +17,5 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        //Se eliminó configuración de botón flotante por defecto (lo mismo en activity_main.xml)
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 }
