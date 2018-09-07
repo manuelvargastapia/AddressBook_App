9a052f18e1065a0c44508d23a98e560bda64f4e0
Manuel <manuel.vargas.t@ug.uchile.cl>
2018-09-07 13:43:44 -0300

    Implementación de métodos de ContentProvider para consultas SQL

--- app/src/main/java/cl/manuel/addressbook_app/Datos/ContentProviderBD.java
+++ app/src/main/java/cl/manuel/addressbook_app/Datos/ContentProviderBD.java
@@ -4,7 +4,10 @@ import android.content.ContentProvider;
 import android.content.ContentValues;
 import android.content.UriMatcher;
 import android.database.Cursor;
+import android.database.SQLException;
+import android.database.sqlite.SQLiteQueryBuilder;
 import android.net.Uri;
+import cl.manuel.addressbook_app.R;
 
 /*DESCRIPCIÓN DE LA CLASE:
 
@@ -47,44 +50,128 @@ public class ContentProviderBD extends ContentProvider {
     //Constructor
     public ContentProviderBD() {}
 
+    //Al crearse este CP
     @Override
     public boolean onCreate() {
 
+        //Crear el BDasistente
+        BDasistente dbAsistente = new BDasistente(getContext());
 
-
-        return false;
+        return true;
     }
 
+    //Método que realiza las consultas y retorna un Cursor que interactúa con los resultados
+    //Este método pide consultas SQL como parámetros, pero sin las palabras clave (WHERE, GROUP BY.etc)
+    //Dicha especificación es realizada por el cuerpo del método
     @Override
-    public String getType(Uri uri) {
-        // TODO: Implement this to handle requests for the MIME type of the data
-        // at the given URI.
-        throw new UnsupportedOperationException("Not yet implemented");
-    }
+    public Cursor query(Uri uri, String[] columnas, String criterioSeleccion,
+                        String[] argumentosSeleccion, String orden) {
 
-    @Override
-    public int delete(Uri uri, String selection, String[] selectionArgs) {
-        // Implement this to handle requests to delete one or more rows.
-        throw new UnsupportedOperationException("Not yet implemented");
+        //Objeto que realiza las consultas
+        SQLiteQueryBuilder queryBuilder = new SQLiteQueryBuilder();
+        queryBuilder.setTables(BDdescripcion.Contacto.NOMBRE_TABLA);
+
+        //Decisión de qué operación realizar
+        switch (uriMatcher.match(uri)){
+            case UN_CONTACTO: //Método que agrega "WHERE ID = [último segmente de query (ej: 5)]"
+                queryBuilder.appendWhere(BDdescripcion.Contacto._ID + "=" + uri.getLastPathSegment());
+                break;
+            case TABLA_CONTACTOS: //No hay WHERE, por lo que se devuelve toda la tabla
+                break;
+            default:
+                throw new UnsupportedOperationException(getContext().getString(R.string.invalid_query_uri) + uri);
+        }
+
+        //Ejecutar query para seleccionar uno o todos los contactos
+        //Nuevamente, este método solo pide consultas sin especificar palabras clave
+        Cursor cursor = queryBuilder.query(bdAsistente.getReadableDatabase(),
+                columnas, criterioSeleccion, argumentosSeleccion, null, null, orden);
+
+        //Ver cambios de contenido
+        cursor.setNotificationUri(getContext().getContentResolver(), uri);
+        return cursor;
     }
 
     @Override
     public Uri insert(Uri uri, ContentValues values) {
-        // TODO: Implement this to handle requests to insert a new row.
-        throw new UnsupportedOperationException("Not yet implemented");
+
+        Uri uriNuevoContacto = null;
+
+        switch (uriMatcher.match(uri)){
+            case TABLA_CONTACTOS:
+                long idFila = bdAsistente.getWritableDatabase().insert(
+                        BDdescripcion.Contacto.NOMBRE_TABLA,
+                        null,
+                        values);
+                if (idFila > 0){
+                    uriNuevoContacto = BDdescripcion.Contacto.crearUriContacto(idFila);
+                    getContext().getContentResolver().notifyChange(uri, null);
+                }else {
+                    throw new SQLException(getContext().getString(R.string.insert_failed) + uri);
+                }
+                break;
+            default:
+                throw new UnsupportedOperationException(getContext().getString(R.string.invalid_insert_uri) + uri);
+        }
+
+        return uriNuevoContacto;
     }
 
     @Override
-    public Cursor query(Uri uri, String[] projection, String selection,
-                        String[] selectionArgs, String sortOrder) {
-        // TODO: Implement this to handle query requests from clients.
-        throw new UnsupportedOperationException("Not yet implemented");
+    public int update(Uri uri, ContentValues columnas, String criterioSeleccion,
+                      String[] argumentosSeleccion) {
+
+        int numeroFilasActualizadas;
+
+        switch (uriMatcher.match(uri)){
+            case UN_CONTACTO:
+                String id = uri.getLastPathSegment();
+                numeroFilasActualizadas = bdAsistente.getWritableDatabase().update(
+                        BDdescripcion.Contacto.NOMBRE_TABLA,
+                        columnas,
+                        BDdescripcion.Contacto._ID + "=" + id,
+                        argumentosSeleccion);
+                break;
+            default:
+                throw new UnsupportedOperationException(getContext().getString(R.string.invalid_update_uri) + uri);
+        }
+
+        if(numeroFilasActualizadas != 0){
+            getContext().getContentResolver().notifyChange(uri, null);
+        }
+
+        return numeroFilasActualizadas;
     }
 
     @Override
-    public int update(Uri uri, ContentValues values, String selection,
-                      String[] selectionArgs) {
-        // TODO: Implement this to handle requests to update one or more rows.
+    public int delete(Uri uri, String criterioSeleccion, String[] argumentosSeleccion) {
+
+        int numeroFilasEliminadas;
+
+        switch (uriMatcher.match(uri)){
+            case UN_CONTACTO:
+                String id = uri.getLastPathSegment();
+                numeroFilasEliminadas = bdAsistente.getWritableDatabase().delete(
+                        BDdescripcion.Contacto.NOMBRE_TABLA,
+                        BDdescripcion.Contacto._ID + "=" + id,
+                        argumentosSeleccion);
+                break;
+            default:
+                throw new UnsupportedOperationException(getContext().getString(R.string.invalid_delete_uri) + uri);
+        }
+
+        if(numeroFilasEliminadas != 0){
+            getContext().getContentResolver().notifyChange(uri, null);
+        }
+
+        return numeroFilasEliminadas;
+    }
+
+    //Requerido, pero no utilizado
+    @Override
+    public String getType(Uri uri) {
+        // TODO: Implement this to handle requests for the MIME type of the data
+        // at the given URI.
         throw new UnsupportedOperationException("Not yet implemented");
     }
 }
